c4bba0d297b9e3d907eff594ec2d97c6ffee8faf
yueh <yueh@yuehde-MacBook-Pro.local>
2023-04-26 20:42:23 +0800

    interview_yhc

--- q1.cpp
+++ q1.cpp
@@ -1,3 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+
+
+int compareString(const string str1, const char* str2){
+    int length_str2 = strlen(str2);
+  
+    for(int i = 0; i < str1.length() && i < length_str2; i++) {
+        if(str1[i] != str2[i]) {
+            return (str1[i] - str2[i]);
+        }
+    }
+    
+    return str1.length() - length_str2;
+}
+
+int binarySearchString(vector<char*> &vs, const string &str) {
+    int lower = 0;
+    int upper = vs.size() - 1; 
+   
+    while (lower <= upper) {
+        int mid = lower + (upper - lower) / 2;
+        int result = compareString(str, vs[mid]);
+          
+        if(result == 0) { 
+            return mid;
+        } else if(result > 0) {
+            lower = mid + 1;
+        } else {
+            upper = mid - 1;
+        }  
+    } 
+    return -1;
+}
+
+
 #include <vector>
 
 std::vector<char *> vs = {
@@ -15,7 +54,7 @@ std::vector<char *> vs = {
 int bin_search(const string &str) {
 
 }
-
+as above.
 // Q2
 // 若要insert "111"到vs內, 應該放在哪最為合適.
-
+看起來是ascending order，放最前面比較適合 
