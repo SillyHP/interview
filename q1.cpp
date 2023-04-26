8f9935a961249a6b270cac6a462608c266ab5a10
yichenghuang <ychuang@bigobject.io>
2023-04-26 15:28:28 +0800

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-std::vector<char *> = {
+std::vector<char *> vs = {
   "aa",
   "ccc",
   "eeeee",
@@ -8,10 +8,14 @@ std::vector<char *> = {
   "zzzz",
 };
 
-int bin_search() {
+// Q1
+// 完成用 binary serach 搜尋vector vs, 回傳字串str在vs中的位置, 若未發現則回傳 -1
+// 不限使用 C或C++的function 
+// 但請注意c++ string 與 c char*轉換的消耗, 盡可能以最有效率的方式完成工作, 
+int bin_search(const string &str) {
 
 }
 
-void rpad(int len, char c) {
+// Q2
+// 若要insert "111"到vs內, 應該放在哪最為合適.
 
-}
